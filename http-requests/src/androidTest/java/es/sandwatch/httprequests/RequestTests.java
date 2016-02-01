be962ddde8365e214e46e943b6acf85b5efaccce
Ismael Alonso <ismaha91@gmail.com>
2016-01-31 18:56:39 -0600

    Add GET request test

--- http-requests/src/androidTest/java/es/sandwatch/httprequests/RequestTests.java
+++ http-requests/src/androidTest/java/es/sandwatch/httprequests/RequestTests.java
@@ -2,6 +2,7 @@ package es.sandwatch.httprequests;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
 import android.support.test.InstrumentationRegistry;
 
@@ -37,7 +38,7 @@ public class RequestTests{
             iConnectivityManagerField.setAccessible(true);
             final Object iConnectivityManager = iConnectivityManagerField.get(conman);
             final Class iConnectivityManagerClass = Class.forName(iConnectivityManager.getClass().getName());
-            final Method setMobileDataEnabledMethod = iConnectivityManagerClass.getDeclaredMethod("setMobileDataEnabled", Boolean.TYPE);
+            final Method setMobileDataEnabledMethod = iConnectivityManagerClass.getDeclaredMethod("setMobileDataEnabled", Boolean.class);
             setMobileDataEnabledMethod.setAccessible(true);
             setMobileDataEnabledMethod.invoke(iConnectivityManager, enabled);
         }
@@ -49,11 +50,20 @@ public class RequestTests{
 
     private void setWifiEnabled(Context context, boolean state){
         WifiManager wifiManager = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+        //The permission required to access this method is declared in the 'androidTest' manifest
         wifiManager.setWifiEnabled(state);
     }
 
+    private boolean isWifiConnected(Context context){
+        WifiManager wifi = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+        ConnectivityManager connManager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+        return wifi.isWifiEnabled() && networkInfo.isConnected();
+    }
+
     @Test()
-    public void NetworkErrorTest(){
+    public void networkErrorTest(){
         setWifiEnabled(InstrumentationRegistry.getContext(), false);
         //setMobileDataEnabled(InstrumentationRegistry.getContext(), false);
 
@@ -82,6 +92,34 @@ public class RequestTests{
         }
 
         setWifiEnabled(InstrumentationRegistry.getContext(), true);
+        while (!isWifiConnected(InstrumentationRegistry.getContext())){
+
+        }
         //setMobileDataEnabled(InstrumentationRegistry.getContext(), true);
+    }
+
+    @Test()
+    public void getTest(){
+        final CountDownLatch signal = new CountDownLatch(1);
+        HttpRequest.get(new HttpRequest.RequestCallback(){
+            @Override
+            public void onRequestComplete(int requestCode, String result){
+                assertTrue(true);
+                signal.countDown();
+            }
+
+            @Override
+            public void onRequestFailed(int requestCode, HttpRequestError error){
+                fail("request shoudln't failed");
+            }
+        }, "http://http-requests.sandwatch.es/api/?text=text");
+
+        try{
+            signal.await();
+        }
+        catch (InterruptedException ix){
+            fail("No interrupt expected");
+            ix.printStackTrace();
+        }
     }
 }
