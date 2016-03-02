c033f9a9bae80da0eeaa9183198218f3f4c78321
Ismael Alonso <ismaha91@gmail.com>
2016-03-02 12:51:13 -0600

    Initial setup for sample

---
+++ sample/src/main/java/es/sandwatch/httprequests/sample/SampleApplication.java
@@ -0,0 +1,17 @@
+package es.sandwatch.httprequests.sample;
+
+import android.app.Application;
+
+
+/**
+ * Sample application class.
+ *
+ * @author Ismael Alonso
+ * @version 1.0.0
+ */
+public class SampleApplication extends Application{
+    @Override
+    public void onCreate(){
+        super.onCreate();
+    }
+}
