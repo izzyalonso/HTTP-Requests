8c7f920a8271cbe9e93ea066c449577bff262745
Ismael Alonso <ismaha91@gmail.com>
2016-03-02 13:26:25 -0600

    Add small sample app

---
+++ sample/src/main/java/es/sandwatch/httprequests/sample/SampleActivity.java
@@ -0,0 +1,90 @@
+package es.sandwatch.httprequests.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import es.sandwatch.httprequests.HttpRequest;
+import es.sandwatch.httprequests.HttpRequestError;
+
+
+/**
+ * Sample activity for HTTP-Requests.
+ *
+ * @author Ismael Alonso
+ * @version 1.0.0
+ */
+public class SampleActivity
+        extends AppCompatActivity
+        implements View.OnClickListener, HttpRequest.RequestCallback{
+
+    private static final String GET_URL = "http://http-requests.sandwatch.es/api/";
+
+
+    private EditText mInput;
+    private Button mSend;
+    private ProgressBar mProgress;
+    private TextView mOutput;
+
+    private int mRequestCode;
+    private boolean mRequestInProgress;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+
+        //Grab UI components
+        mInput = (EditText)findViewById(R.id.sample_input);
+        mSend = (Button)findViewById(R.id.sample_button);
+        mProgress = (ProgressBar)findViewById(R.id.sample_progress);
+        mOutput = (TextView)findViewById(R.id.sample_output);
+
+        mSend.setOnClickListener(this);
+
+        mRequestCode = -1;
+        mRequestInProgress = false;
+    }
+
+    @Override
+    public void onClick(View view){
+        if (view.getId() == R.id.sample_button){
+            if (!mRequestInProgress){
+                String input = mInput.getText().toString().trim();
+                mRequestCode = HttpRequest.get(this, GET_URL + "?text=" + input);
+                mProgress.setVisibility(View.VISIBLE);
+                mSend.setText(R.string.button_cancel);
+                mOutput.setText("");
+            }
+            else{
+                HttpRequest.cancel(mRequestCode);
+                mRequestCode = -1;
+                mProgress.setVisibility(View.INVISIBLE);
+                mSend.setText(R.string.button_send);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestComplete(int requestCode, String result){
+        if (requestCode == mRequestCode){
+            mOutput.setText(result);
+            mProgress.setVisibility(View.INVISIBLE);
+            mSend.setText(R.string.button_send);
+        }
+    }
+
+    @Override
+    public void onRequestFailed(int requestCode, HttpRequestError error){
+        if (requestCode == mRequestCode){
+            mOutput.setText("There was an error");
+            mProgress.setVisibility(View.INVISIBLE);
+            mSend.setText(R.string.button_send);
+        }
+    }
+}
--- sample/src/main/java/es/sandwatch/httprequests/sample/SampleApplication.java
+++ sample/src/main/java/es/sandwatch/httprequests/sample/SampleApplication.java
@@ -2,9 +2,11 @@ package es.sandwatch.httprequests.sample;
 
 import android.app.Application;
 
+import es.sandwatch.httprequests.HttpRequest;
+
 
 /**
- * Sample application class.
+ * Sample application class for HTTP-Requests.
  *
  * @author Ismael Alonso
  * @version 1.0.0
@@ -13,5 +15,11 @@ public class SampleApplication extends Application{
     @Override
     public void onCreate(){
         super.onCreate();
+        //Always initialize the class in the application
+        HttpRequest.init(this);
+        //Add as many headers and url parameters as you desire, they will be sent
+        //  in every request untill you remove them
+        HttpRequest.addHeader("version", "1");
+        HttpRequest.addUrlParameter("test", "true");
     }
 }
